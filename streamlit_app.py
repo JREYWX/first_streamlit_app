d26f5bac989240362283e6f4a686172dd8d920ae
JREYWX <99484545+JREYWX@users.noreply.github.com>
2022-05-16 14:01:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 # New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('View Our Fruit List - Add Your Favorites!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like informaition about?')
   if not fruit_choice:
